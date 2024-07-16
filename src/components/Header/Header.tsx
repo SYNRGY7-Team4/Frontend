0f0252de796e52cd076f0b4937b77f4d0a4885de
faizakmalz <faztcollege@gmail.com>
2024-07-16 22:35:09 +0700

    feat: atur pin

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,9 +2,13 @@ import logo from "@/assets/logo.png"
 
 export default function Header() {
   return (
-    <header className='px-8 h-[73px] md:px-14 md:h-[94px] flex items-center bg-neutral-01'>
-      <div className='w-[78px] md:w-[120px]'>
-        <img src={logo} alt='Lumi Logo' />
+    <header className='px-8 h-[73px] md:px-[83px] md:h-[94px] flex items-center bg-neutral-01'>
+      <div className='w-full flex justify-between'>
+        <img src={logo} alt='Lumi Logo' className="w-[78px] md:w-[120px]"  />
+        <div className="flex items-center gap-8 font-bold text-[24px] text-[#1C1C1C]">
+          <p>Bantuan</p>
+          <p>Info Perusahaan</p>
+        </div>
       </div>
     </header>
   )
