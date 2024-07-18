1ed0ea9c8f0b549c6f50c45321aebfe9ddb9cf28
smollMushroom <fadhilahfarhan2003@gmail.com>
2024-07-18 12:49:19 +0700

    feat: add toggle visibility eye icon to password inputs

--- src/pages/Register/Password.tsx
+++ src/pages/Register/Password.tsx
@@ -4,8 +4,31 @@ import Header from "@/components/Header/Header"
 import Footer from "@/components/Footer/Footer"
 import Input from "@/components/Input/Input"
 import Label from "@/components/Label/Label"
+import eyeOpen from "@/assets/eye-open.svg"
+import eyeClose from "@/assets/eye-closed.svg"
+import { useState } from "react"
+import { useNavigate } from "react-router-dom"
 
 export default function Password() {
+  const [visibility, setVisibility] = useState({
+    password: false,
+    confirmPassword: false
+  });
+
+  const toggleVisibility = (event: React.MouseEvent<HTMLButtonElement>, field: 'password' | 'confirmPassword') => {
+    event.preventDefault()
+
+    setVisibility(prevVisibility => ({
+      ...prevVisibility,
+      [field]: !prevVisibility[field]
+    }));
+  }
+
+  const navigate = useNavigate();
+  const next = () => {
+    navigate('/register/ktp');
+  }
+
   return (
     <>
       <Header />
@@ -22,25 +45,47 @@ export default function Password() {
               <div className='flex flex-col gap-y-3'>
                 <div className='flex flex-col gap-y-1'>
                   <Label htmlFor='password'>Password</Label>
-                  <Input
-                    type='password'
-                    id='password'
-                    placeholder='Password'
-                    aria-label='Masukkan Password Anda'
-                  />
+                  <div className="flex relative">
+                    <Input
+                      className="w-full bg-neutral-02 py-3 pl-5 pr-10 rounded-lg focus:outline-primary-blue"
+                      type={visibility.password ? 'text' : 'password'}
+                      id='password'
+                      placeholder='Password'
+                      aria-label='Masukkan Password Anda'
+                      required
+                    />
+                    <Button onClick={(event) => toggleVisibility(event, 'password')} className="absolute right-[10px] flex items-center w-fit h-full hover:shadow-none bg-transparent">
+                      <img 
+                        src={visibility.password ? eyeClose : eyeOpen} 
+                        alt="eye-open" 
+                        className="w-[25px] cursor-pointer z-1"/>
+                    </Button>
+                  </div>
                 </div>
                 <div className='flex flex-col gap-y-1'>
                   <Label htmlFor='confirm-password'>Ulangi Password</Label>
-                  <Input
-                    type='password'
-                    id='confirm-password'
-                    placeholder='Ulangi Password'
-                    aria-label='Masukkan Ulang password Anda'
-                  />
+                  <div className="flex relative">
+                    <Input
+                      className="w-full bg-neutral-02 py-3 pl-5 pr-10 rounded-lg focus:outline-primary-blue"
+                      type={visibility.confirmPassword? 'text' : 'password'}
+                      id='confirm-password'
+                      placeholder='Ulangi Password'
+                      aria-label='Masukkan Ulang Password Anda'
+                      required
+                    />
+                    <Button onClick={(event) => toggleVisibility(event, 'confirmPassword')} className="absolute right-[10px] flex items-center w-fit h-full hover:shadow-none bg-transparent">
+                      <img 
+                        src={visibility.confirmPassword ? eyeClose : eyeOpen} 
+                        alt="eye-open" 
+                        className="w-[25px] cursor-pointer z-1"/>
+                    </Button>
+                  </div>
                 </div>
               </div>
               <div className='flex flex-col items-center'>
-                <Button aria-label='Tombol Lanjut' className='my-9'>Lanjut</Button>
+                <Button aria-label='Tombol Lanjut' className='my-9' onClick={next}>
+                  Lanjut
+                </Button>
               </div>
             </form>
           </div>
