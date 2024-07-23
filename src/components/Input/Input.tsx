cf0bc3ae0b9f9d22bf7c11d07fd34fbe83e6c31c
YYanti00 <lindayanti928@gmail.com>
2024-07-23 17:35:22 +0700

    refactor: input component to use forwardRef

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,12 +1,19 @@
+import React from "react";
 type InputProps = React.InputHTMLAttributes<HTMLInputElement> & {
   className?: string;
 };
 
-export default function Input({ className, ...props }: InputProps) {
+const Input: React.ForwardRefRenderFunction<HTMLInputElement, InputProps> = (
+  { className, ...props },
+  ref
+) => {
   return (
     <input
       className={`w-full bg-neutral-02 py-3 px-5 rounded-lg focus:outline-primary-blue ${className}`}
+      ref={ref}
       {...props}
     />
   );
-}
+};
+
+export default React.forwardRef(Input);
