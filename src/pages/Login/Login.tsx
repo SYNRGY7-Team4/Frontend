b2505d094378295aed10df30ec9c538dd9416a0f
fajarriskihidayat <fajarrh2002@gmail.com>
2024-07-27 15:19:46 +0700

    feat: integration api in login page

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,13 +1,14 @@
-import bgAuth from "@/assets/bg-auth.jpg"
-import { Link } from "react-router-dom"
-import Button from "@/components/Button/Button"
-import Header from "@/components/Header/Header"
-import Footer from "@/components/Footer/Footer"
-import Input from "@/components/Input/Input"
-import Label from "@/components/Label/Label"
-import { z } from "zod"
-import { useForm } from "react-hook-form"
-import { zodResolver } from "@hookform/resolvers/zod"
+import bgAuth from "@/assets/bg-auth.jpg";
+import { Link } from "react-router-dom";
+import Button from "@/components/Button/Button";
+import Header from "@/components/Header/Header";
+import Footer from "@/components/Footer/Footer";
+import Input from "@/components/Input/Input";
+import Label from "@/components/Label/Label";
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import axios from "axios";
 
 const loginSchema = z.object({
   email: z
@@ -23,9 +24,9 @@ const loginSchema = z.object({
       new RegExp(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,15}$/),
       "Password harus terdiri dari 8-15 karakter dan harus mengandung kombinasi huruf dan angka"
     ),
-})
+});
 
-type TLoginSchema = z.infer<typeof loginSchema>
+type TLoginSchema = z.infer<typeof loginSchema>;
 
 export default function Login() {
   const {
@@ -35,37 +36,46 @@ export default function Login() {
     reset,
   } = useForm<TLoginSchema>({
     resolver: zodResolver(loginSchema),
-  })
+  });
 
-  const onSubmit = (data: TLoginSchema) => {
-    console.log(data)
-    reset()
-  }
+  const onSubmit = async (data: TLoginSchema) => {
+    try {
+      const res = await axios.post(
+        "https://backend-dev-synrgy-team4.koyeb.app/api/auth/login",
+        data
+      );
+
+      console.log({ data: res.data });
+      reset();
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <>
       <Header />
       <main
-        className='w-full min-h-[828px] h-[calc(100vh-73px-75px)] md:h-[calc(100vh-94px-75px)] bg-cover bg-center'
+        className="w-full min-h-[828px] h-[calc(100vh-73px-75px)] md:h-[calc(100vh-94px-75px)] bg-cover bg-center"
         style={{ backgroundImage: `url(${bgAuth})` }}
       >
-        <div className='container mx-auto px-6 flex items-center justify-center md:justify-end h-full'>
-          <div className='bg-neutral-01 px-8 py-14 md:px-14 rounded-lg w-[450px]'>
-            <h1 className='mb-10 text-3xl text-primary-blue font-bold'>
+        <div className="container mx-auto px-6 flex items-center justify-center md:justify-end h-full">
+          <div className="bg-neutral-01 px-8 py-14 md:px-14 rounded-lg w-[450px]">
+            <h1 className="mb-10 text-3xl text-primary-blue font-bold">
               Selamat Datang,
             </h1>
             <form
-              className='flex flex-col gap-y-8'
+              className="flex flex-col gap-y-8"
               onSubmit={handleSubmit(onSubmit)}
             >
-              <div className='flex flex-col gap-y-3'>
-                <div className='flex flex-col gap-y-1'>
-                  <Label htmlFor='email'>Email</Label>
+              <div className="flex flex-col gap-y-3">
+                <div className="flex flex-col gap-y-1">
+                  <Label htmlFor="email">Email</Label>
                   <Input
-                    type='email'
-                    id='email'
-                    placeholder='*****@email.com'
-                    aria-label='Masukkan email Anda'
+                    type="email"
+                    id="email"
+                    placeholder="*****@email.com"
+                    aria-label="Masukkan email Anda"
                     {...register("email")}
                     className={`${
                       errors.email
@@ -75,20 +85,20 @@ export default function Login() {
                   />
                   {errors.email && (
                     <span
-                      className='text-red-500 text-sm'
+                      className="text-red-500 text-sm"
                       aria-label={errors.email.message}
                     >
                       {errors.email.message}
                     </span>
                   )}
                 </div>
-                <div className='flex flex-col gap-y-1'>
-                  <Label htmlFor='password'>Password</Label>
+                <div className="flex flex-col gap-y-1">
+                  <Label htmlFor="password">Password</Label>
                   <Input
-                    type='password'
-                    id='password'
-                    placeholder='Password'
-                    aria-label='Masukkan password Anda'
+                    type="password"
+                    id="password"
+                    placeholder="Password"
+                    aria-label="Masukkan password Anda"
                     {...register("password")}
                     className={`${
                       errors.password
@@ -98,31 +108,31 @@ export default function Login() {
                   />
                   {errors.password && (
                     <span
-                      className='text-red-500 text-sm'
+                      className="text-red-500 text-sm"
                       aria-label={errors.password.message}
                     >
                       {errors.password.message}
                     </span>
                   )}
                 </div>
-                <div className='flex justify-end'>
+                <div className="flex justify-end">
                   <Link
-                    to='/forgot-password'
-                    className='text-primary-blue'
-                    aria-label='Link menuju halaman lupa password'
+                    to="/forgot-password"
+                    className="text-primary-blue"
+                    aria-label="Link menuju halaman lupa password"
                   >
                     Lupa password?
                   </Link>
                 </div>
               </div>
-              <div className='flex flex-col gap-y-2 items-center'>
-                <Button aria-label='Tombol masuk'>Masuk</Button>
+              <div className="flex flex-col gap-y-2 items-center">
+                <Button aria-label="Tombol masuk">Masuk</Button>
                 <p>
                   Belum punya akun?{" "}
                   <Link
-                    to='/register'
-                    className='text-primary-blue'
-                    aria-label='Link menuju halaman register'
+                    to="/register"
+                    className="text-primary-blue"
+                    aria-label="Link menuju halaman register"
                   >
                     Registrasi
                   </Link>
@@ -134,5 +144,5 @@ export default function Login() {
       </main>
       <Footer />
     </>
-  )
+  );
 }
