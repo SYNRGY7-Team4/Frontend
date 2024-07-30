4efd4e6fc41132dd52630a3af4cbb31640e6043e
Mhd. Zulfikar Pinem <zulfikarm022@gmail.com>
2024-07-30 22:54:09 +0700

    feat(SaldoCard): add visibility toggle for account number and balance

---
+++ src/utils/currencyFormat.ts
@@ -0,0 +1,11 @@
+const currencyFormat = (balance: number, locale: string, currency: string) => {
+  const result = new Intl.NumberFormat(locale, {
+    style: "currency",
+    currency: currency,
+    maximumFractionDigits: 0,
+  }).format(balance)
+
+  return result
+}
+
+export default currencyFormat
