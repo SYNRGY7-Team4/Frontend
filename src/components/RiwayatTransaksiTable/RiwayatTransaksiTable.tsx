6474d0eeee546393d798727c319d7a88ac2f882a
YYanti00 <lindayanti928@gmail.com>
2024-08-02 12:17:02 +0700

    refactor: modify RiwayatTransaksiTable component

--- src/components/RiwayatTransaksiTable/RiwayatTransaksiTable.tsx
+++ src/components/RiwayatTransaksiTable/RiwayatTransaksiTable.tsx
@@ -1,5 +1,4 @@
-import { Link } from "react-router-dom"
-import noDocuments from "@/assets/no_documents.svg"
+import noDocuments from "@/assets/no_documents.svg";
 
 const transactions = [
   {
@@ -34,69 +33,55 @@ const transactions = [
     mutation: "- Rp. 10.000",
     balance: "Rp. 480.000",
   },
-]
+];
 
 export default function RiwayatTransaksiTable() {
   return (
-    <div className='w-full bg-neutral-01 p-6 rounded-lg shadow-02 flex flex-col gap-y-5'>
-      <div className='flex flex-col items-start sm:flex-row sm:items-center justify-between gap-3'>
-        <h2 className='text-2xl font-bold'>Riwayat Transaksi</h2>
-        <Link
-          to='#'
-          className='bg-primary-blue text-neutral-01 py-2 px-5 rounded-full'
-        >
-          Lihat Selengkapnya
-        </Link>
-      </div>
-      <div className='overflow-auto'>
-        <table className='w-full'>
-          <thead>
-            <tr>
-              <th className='py-3 px-6 bg-neutral-100 text-left'>Tanggal</th>
-              <th className='py-3 px-6 bg-neutral-100 text-left'>Deskripsi</th>
-              <th className='py-3 px-6 bg-neutral-100 text-left'>Kategori</th>
-              <th className='py-3 px-6 bg-neutral-100 text-left'>Mutasi</th>
-              <th className='py-3 px-6 bg-neutral-100 text-left'>Saldo</th>
-            </tr>
-          </thead>
-          <tbody>
-            {transactions.length > 0 ? (
-              transactions.map((transaction) => (
-                <tr
-                  key={transaction.id}
-                  className='border-b border-neutral-200'
-                >
-                  <td className='py-4 px-6'>{transaction.date}</td>
-                  <td className='py-4 px-6'>{transaction.description}</td>
-                  <td className='py-4 px-6'>
-                    <span className='py-0.5 px-2 rounded-full bg-neutral-200'>
-                      {transaction.category}
-                    </span>
-                  </td>
-                  <td className='py-4 px-6'>
-                    <span className='text-secondary-red font-bold'>
-                      {transaction.mutation}
-                    </span>
-                  </td>
-                  <td className='py-4 px-6'>
-                    <span className='font-bold'>{transaction.balance}</span>
-                  </td>
-                </tr>
-              ))
-            ) : (
-              <tr>
-                <td colSpan={5} className='py-4'>
-                  <img
-                    src={noDocuments}
-                    className='block mx-auto'
-                    alt='Tidak ada riwayat transaksi'
-                  />
+    <div className="overflow-auto">
+      <table className="w-full">
+        <thead>
+          <tr>
+            <th className="py-3 px-6 bg-neutral-100 text-left">Tanggal</th>
+            <th className="py-3 px-6 bg-neutral-100 text-left">Deskripsi</th>
+            <th className="py-3 px-6 bg-neutral-100 text-left">Kategori</th>
+            <th className="py-3 px-6 bg-neutral-100 text-left">Mutasi</th>
+            <th className="py-3 px-6 bg-neutral-100 text-left">Saldo</th>
+          </tr>
+        </thead>
+        <tbody>
+          {transactions.length > 0 ? (
+            transactions.map((transaction) => (
+              <tr key={transaction.id} className="border-b border-neutral-200">
+                <td className="py-4 px-6">{transaction.date}</td>
+                <td className="py-4 px-6">{transaction.description}</td>
+                <td className="py-4 px-6">
+                  <span className="py-0.5 px-2 rounded-full bg-neutral-200">
+                    {transaction.category}
+                  </span>
+                </td>
+                <td className="py-4 px-6">
+                  <span className="text-secondary-red font-bold">
+                    {transaction.mutation}
+                  </span>
+                </td>
+                <td className="py-4 px-6">
+                  <span className="font-bold">{transaction.balance}</span>
                 </td>
               </tr>
-            )}
-          </tbody>
-        </table>
-      </div>
+            ))
+          ) : (
+            <tr>
+              <td colSpan={5} className="py-4">
+                <img
+                  src={noDocuments}
+                  className="block mx-auto"
+                  alt="Tidak ada riwayat transaksi"
+                />
+              </td>
+            </tr>
+          )}
+        </tbody>
+      </table>
     </div>
-  )
+  );
 }
