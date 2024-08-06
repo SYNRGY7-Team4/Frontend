053c609e370c99411e695c47339e3b4090e684b3
YYanti00 <lindayanti928@gmail.com>
2024-08-06 12:55:16 +0700

    refactor: add any type in transaction

--- src/components/RiwayatTransaksiTable/RiwayatTransaksiTable.tsx
+++ src/components/RiwayatTransaksiTable/RiwayatTransaksiTable.tsx
@@ -19,35 +19,35 @@ export default function RiwayatTransaksiTable() {
   };
 
   return (
-    <div className='overflow-auto'>
-      <table className='w-full'>
+    <div className="overflow-auto">
+      <table className="w-full">
         <thead>
           <tr>
-            <th className='py-3 px-6 bg-neutral-100 text-left'>Tanggal</th>
-            <th className='py-3 px-6 bg-neutral-100 text-left'>Deskripsi</th>
-            <th className='py-3 px-6 bg-neutral-100 text-left'>Kategori</th>
-            <th className='py-3 px-6 bg-neutral-100 text-left'>Mutasi</th>
-            <th className='py-3 px-6 bg-neutral-100 text-left'>Saldo</th>
+            <th className="py-3 px-6 bg-neutral-100 text-left">Tanggal</th>
+            <th className="py-3 px-6 bg-neutral-100 text-left">Deskripsi</th>
+            <th className="py-3 px-6 bg-neutral-100 text-left">Kategori</th>
+            <th className="py-3 px-6 bg-neutral-100 text-left">Mutasi</th>
+            <th className="py-3 px-6 bg-neutral-100 text-left">Saldo</th>
           </tr>
         </thead>
         <tbody>
           {userMutations.length > 0 ? (
-            userMutations.map((transaction) => (
-              <tr key={transaction.id} className='border-b border-neutral-200'>
-                <td className='py-4 px-6'>{transaction.datetime.toString()}</td>
-                <td className='py-4 px-6'>{transaction.description}</td>
-                <td className='py-4 px-6'>
-                  <span className='py-0.5 px-2 rounded-full bg-neutral-200'>
+            userMutations.map((transaction: any) => (
+              <tr key={transaction.id} className="border-b border-neutral-200">
+                <td className="py-4 px-6">{transaction.datetime.toString()}</td>
+                <td className="py-4 px-6">{transaction.description}</td>
+                <td className="py-4 px-6">
+                  <span className="py-0.5 px-2 rounded-full bg-neutral-200">
                     {transaction.type}
                   </span>
                 </td>
-                <td className='py-4 px-6'>
-                  <span className='text-secondary-red font-bold'>
+                <td className="py-4 px-6">
+                  <span className="text-secondary-red font-bold">
                     - {transaction.amount}
                   </span>
                 </td>
-                <td className='py-4 px-6'>
-                  <span className='font-bold'>
+                <td className="py-4 px-6">
+                  <span className="font-bold">
                     {calculateBalance(transaction.amount)}
                   </span>
                 </td>
@@ -55,11 +55,11 @@ export default function RiwayatTransaksiTable() {
             ))
           ) : (
             <tr>
-              <td colSpan={5} className='py-4'>
+              <td colSpan={5} className="py-4">
                 <img
                   src={noDocuments}
-                  className='block mx-auto'
-                  alt='Tidak ada riwayat transaksi'
+                  className="block mx-auto"
+                  alt="Tidak ada riwayat transaksi"
                 />
               </td>
             </tr>
--- src/components/TransaksiTerbaruCard/TransaksiTerbaruCard.tsx
+++ src/components/TransaksiTerbaruCard/TransaksiTerbaruCard.tsx
@@ -8,29 +8,29 @@ export default function TransaksiTerbaruCard() {
   const latestTransactions = userMutations.slice(-2);
 
   return (
-    <div className='w-full h-full bg-neutral-01 p-6 rounded-lg shadow-02 flex flex-col gap-y-2.5'>
-      <h2 className='text-2xl font-bold'>Transaksi Terbaru</h2>
-      <div className='h-full flex flex-col gap-4'>
+    <div className="w-full h-full bg-neutral-01 p-6 rounded-lg shadow-02 flex flex-col gap-y-2.5">
+      <h2 className="text-2xl font-bold">Transaksi Terbaru</h2>
+      <div className="h-full flex flex-col gap-4">
         {latestTransactions.length > 0 ? (
-          latestTransactions.map((transaction) => (
+          latestTransactions.map((transaction: any) => (
             <div
               key={transaction.id}
-              className='flex items-center bg-neutral-01 border border-primary-darkBlue rounded-lg py-4 px-5 gap-4 overflow-auto'
+              className="flex items-center bg-neutral-01 border border-primary-darkBlue rounded-lg py-4 px-5 gap-4 overflow-auto"
             >
               <img
                 src={avatar}
-                className='w-10 h-10'
+                className="w-10 h-10"
                 alt={`Avatar ${transaction.account_to}`}
               />
-              <div className=''>
-                <p className='text-lg'>{transaction.account_to}</p>
-                <p className='text-gray-500'>{transaction.account_to}</p>
+              <div className="">
+                <p className="text-lg">{transaction.account_to}</p>
+                <p className="text-gray-500">{transaction.account_to}</p>
               </div>
             </div>
           ))
         ) : (
-          <div className='flex justify-center'>
-            <img src={noCreditCard} alt='tidak ada transaksi' />
+          <div className="flex justify-center">
+            <img src={noCreditCard} alt="tidak ada transaksi" />
           </div>
         )}
       </div>
