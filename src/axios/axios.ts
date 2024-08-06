192fbd64ea9855c01cffdebdc1e8a78c846181be
Mhd. Zulfikar Pinem <zulfikarm022@gmail.com>
2024-08-06 11:44:58 +0700

    feat: add refresh token feature

--- src/axios/axios.ts
+++ src/axios/axios.ts
@@ -20,4 +20,38 @@ axiosInstance.interceptors.request.use(
   }
 );
 
+// REFRESH TOKEN INTERCEPTOR
+// axiosInstance.interceptors.response.use(
+//   (res) => {
+//     return res;
+//   },
+//   async (err) => {
+//     const originalConfig = err.config;
+
+//     if (originalConfig.url !== "/auth/login" && err.response) {
+//       if (err.response.status === 401 && !originalConfig._retry) {
+//         originalConfig._retry = true;
+
+//         try {
+//           const response = await axiosInstance.post("/auth/refresh-token", {
+//             refreshToken: localStorage.getItem("refreshToken"),
+//           });
+
+//           const { jwt_token } = response.data;
+//           localStorage.setItem("token", jwt_token);
+//           axiosInstance.defaults.headers[
+//             "Authorization"
+//           ] = `Bearer ${jwt_token}`;
+
+//           return axiosInstance(originalConfig);
+//         } catch (_error) {
+//           return Promise.reject(_error);
+//         }
+//       }
+//     }
+
+//     return Promise.reject(err);
+//   }
+// );
+
 export default axiosInstance;
