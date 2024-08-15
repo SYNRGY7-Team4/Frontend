45c8c36347c11e458abf0ffd22834cb8b92fb38b
YYanti00 <lindayanti928@gmail.com>
2024-08-15 20:11:20 +0700

    refactor: change baseURL with new api url

--- src/axios/axios.ts
+++ src/axios/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "https://backend-dev-synrgy-team4.koyeb.app/api",
+  baseURL: "https://lumibank-backend-edqo6jv53q-et.a.run.app/api",
   headers: {
     "Content-Type": "application/json",
   },
